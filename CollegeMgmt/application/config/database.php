6b3ddd99fb977ce4e9b4123cbe68f93dd984cfc2
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-11-05 18:49:04 +0530

    Update database.php

--- CollegeMgmt/application/config/database.php
+++ CollegeMgmt/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'ubuntu',
-	'password' => 'Ubuntu@123',
+	'hostname' => 'cloudtestingdb.crgya7bbq8o7.ap-southeast-1.rds.amazonaws.com',
+	'username' => 'admin',
+	'password' => 'admin123',
 	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
