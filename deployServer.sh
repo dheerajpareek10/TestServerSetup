21518c0c3f3e09ad1cc42a9e1bf8840620cbd28f
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-11-13 14:32:59 +0530

    Update deployServer.sh

--- deployServer.sh
+++ deployServer.sh
@@ -11,11 +11,6 @@ sudo ufw status
 echo "y" | sudo ufw enable
 sudo ufw status
 
-# Install MySQL Server
-sudo apt install mysql-server -y
-sudo chmod +x secureMySQL.sh
-sh secureMySQL.sh
-
 # Install PHP 
 sudo apt install php libapache2-mod-php php-mysql -y
 php -v
@@ -29,18 +24,6 @@ sudo cp apache2.conf /etc/apache2/apache2.conf
 sudo rm /etc/apache2/apache2_old.conf
 sudo systemctl restart apache2
 
-sudo chmod 777 -R /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld_old.cnf
-sudo cp mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo rm /etc/mysql/mysql.conf.d/mysqld_old.cnf
-sudo systemctl restart mysql
-
 # Place the code in the server html folder
 sudo chmod 777 -R /var/www/html
 sudo mv CollegeMgmt /var/www/html
-
-# Create ubuntu user in mysql, give privileges and create college_mgmt database
-sh createUser.sh
-
-# Dump data in college_mgmt db
-sudo mysql -u ubuntu -pUbuntu@123 college_mgmt < updateDB.sql
